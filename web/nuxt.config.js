ad72badeab6e8c490d79dbbc64e260bf02cde5ee
lx0427 <lx15172413095@163.com>
2020-01-09 13:50:09 +0800

    fix: 关闭nuxt中eslint检测

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -44,7 +44,7 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    // '@nuxtjs/eslint-module',
     '@nuxtjs/vuetify'
   ],
   /*
